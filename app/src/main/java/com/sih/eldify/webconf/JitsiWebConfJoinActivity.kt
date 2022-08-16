c7fdbbcf54d16a506039ab9fbf0c53f1be4a1958
Ansh Gaikwad <anshyg2002@gmail.com>
2022-08-16 21:05:06 +0530

    added websocket, webconf, camera packages

---
+++ app/src/main/java/com/sih/eldify/webconf/JitsiWebConfJoinActivity.kt
@@ -0,0 +1,26 @@
+package com.sih.eldify.webconf
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.sih.eldify.R
+import kotlinx.android.synthetic.main.activity_jitsi_web_conf_join.*
+import org.jitsi.meet.sdk.JitsiMeetActivity
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions
+
+class JitsiWebConfJoinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_jitsi_web_conf_join)
+    }
+
+    fun onButtonClick(view: View) {
+        val text=conferenceName.text.toString()
+        if (text.isNotEmpty()){
+            val options= JitsiMeetConferenceOptions.Builder()
+                .setRoom(text)
+                .build()
+            JitsiMeetActivity.launch(this,options)
+        }
+    }
+}
