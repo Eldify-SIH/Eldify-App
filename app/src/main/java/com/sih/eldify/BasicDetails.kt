691fdf295779693968f507d70b5a21e70952c544
Ansh Gaikwad <anshyg2002@gmail.com>
2022-08-16 22:37:52 +0530

    Basic Details simple UI coded

---
+++ app/src/main/java/com/sih/eldify/BasicDetails.kt
@@ -0,0 +1,11 @@
+package com.sih.eldify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BasicDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_basic_details)
+    }
+}
